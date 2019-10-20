50bab0bd20a17e4767923bc5b2aca53c86fd15fb
drpika1134 <drpika1134@gmail.com>
2019-10-20 14:49:50 -0600

    stores tab in localStorage

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  // Display the DataTable
   $('.logs').DataTable()
+
+  // Stores/remember the tab in localStorage when redirect
+  $('a[data-toggle="tab"]').click(function(e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    let id = $(e.target).attr('href')
+    localStorage.setItem('selectedTab', id)
+  })
+
+  let selectedTab = localStorage.getItem('selectedTab')
+  if (selectedTab != null) {
+    $('a[data-toggle="tab"][href="' + selectedTab + '"]').tab('show')
+  }
 })
